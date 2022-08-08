20d38073fe47fb3d8cad1992b55a9eddfd7f9670
vasiliyChobot <i@vchobot.ru>
2022-08-08 13:16:38 +0300

    add simple test

---
+++ src/test/java/DifferTest.java
@@ -0,0 +1,29 @@
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DifferTest {
+    public static String getExpected(String fileName) throws IOException {
+        return Files.readString(Path.of("./src/test/resources/expected"));
+    }
+    @Test
+    public void differNormalTest() throws IOException {
+        var actual = Differ.generate("/home/vasiliy/file1.json", "/home/vasiliy/file2.json");
+        var expected = """
+                {
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                }""";
+        assertThat(actual).isEqualTo(expected);
+    }
+}
